62a8ec6b72242734f16cff264f95b69b414f054d
jeronimo_nuñez <jeronimonunez96@gmail.com>
2024-04-30 21:27:22 -0300

    Modificacion del nav

--- Js/menu-mobile.js
+++ Js/menu-mobile.js
@@ -1,11 +1,24 @@
-let btnOpenMenu = document.querySelector("#open-menu");
-let btnCloseMenu = document.querySelector("#close-menu");
-let menu = document.querySelector("#mobile-menu");  
+// let btnOpenMenu = document.querySelector("#open-menu");
+// let btnCloseMenu = document.querySelector("#close-menu");
+// let menu = document.querySelector("#mobile-menu");  
 
-btnOpenMenu.addEventListener("click", () => {
-    menu.classList.remove("disabled");
-});
+// btnOpenMenu.addEventListener("click", () => {
+//     menu.classList.remove("disabled");
+// });
 
-btnCloseMenu.addEventListener("click", () => {
-    menu.classList.add("disabled");
-});
+// btnCloseMenu.addEventListener("click", () => {
+//     menu.classList.add("disabled");
+// });
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
