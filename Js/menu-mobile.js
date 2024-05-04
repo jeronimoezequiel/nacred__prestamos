22263f8267639c897632ff41de54aa108dbbcbbf
jeronimo_nuñez <jeronimonunez96@gmail.com>
2024-05-04 13:16:54 -0300

    Pagina terminada

--- Js/menu-mobile.js
+++ Js/menu-mobile.js
@@ -1,15 +1,4 @@
-// let btnOpenMenu = document.querySelector("#open-menu");
-// let btnCloseMenu = document.querySelector("#close-menu");
-// let menu = document.querySelector("#mobile-menu");  
-
-// btnOpenMenu.addEventListener("click", () => {
-//     menu.classList.remove("disabled");
-// });
-
-// btnCloseMenu.addEventListener("click", () => {
-//     menu.classList.add("disabled");
-// });
-
+//Menu Hamburguesa 
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
